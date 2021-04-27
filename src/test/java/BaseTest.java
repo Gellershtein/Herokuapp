c00b0745d825078a498fd7198e72b8692cf46a37
Gellershtein <63881048+Gellershtein@users.noreply.github.com>
2021-04-27 20:56:19 +0300

    HW.Checkboxes

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,10 +1,15 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public abstract class BaseTest {
     WebDriver driver;
@@ -18,8 +23,20 @@ public abstract class BaseTest {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() {
-        driver.quit();
+//    @AfterMethod(alwaysRun = true)
+//    public void tearDown() {
+//        driver.quit();
+//    }
+
+    public void logIn(WebDriver driver) {
+        driver.get("http://the-internet.herokuapp.com/login");
+        WebElement userName = driver.findElement(By.id("username"));
+        userName.sendKeys("tomsmith");
+        assertEquals(userName.getAttribute("value"), "tomsmith", "Юзер не был введен");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        userName.submit();
+        boolean logOutButton = driver.findElement(By.cssSelector(".secondary")).isDisplayed();
+        assertTrue(logOutButton,
+                "Аккаунт не залогировался");
     }
 }
